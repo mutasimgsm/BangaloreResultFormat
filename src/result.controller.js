40260f955c0472729dd4d8b8246746b6e0e19302
Mutasim <gsm@yahoo.com>
2017-02-02 01:36:00 +0530

    Added Graph

--- src/result.controller.js
+++ src/result.controller.js
@@ -10,8 +10,6 @@ ResultController.$inject = ["DataService"];
 function ResultController(DataService, $scope) {
   var get = this;
 
-  get.parseInt = parseInt;
-
   var promise = DataService.getData();
   promise.then(function (response) {
     get.results = response.data;
@@ -20,6 +18,8 @@ function ResultController(DataService, $scope) {
   .catch(function (error) {
     console.log("Something is going wrrong!!", error)
   });
+  var marksArray = [];
+  var labelsArray = [];
 
   get.getResult = function (search) {
     var total = 0;
@@ -27,66 +27,92 @@ function ResultController(DataService, $scope) {
     var len = get.results.main.length;
     console.log("len : ", len);
 
-
     for (var i = 0; i < len; i++) {
       console.log("I's increament", i);
         for (var j = 0; j < get.results.main[i].length; j++) {
-
-          console.log("j's length",get.results.main[i].length);
-
             var regno = get.results.main[i][j].regno;
-            // console.log("Inside j : ",regno);
-
 
             if((search.indexOf(regno)!== -1 )&&
             (search.length === regno.length)) {
 
-                // total += parseInt(get.results.main[i][j+1].marks);
-                // console.log("Total of marks is: ", total);
-
                 get.result = get.results.main[i];
-                // console.log("Index of innerloop", get.result);
                 get.name = get.results.main[i][j].name;
                 get.regno = get.results.main[i][j].regno;
+
+                for (var m in get.results.main[j]) {
+                 marksArray.push(get.results.main[i][m].marks);
+                 labelsArray.push(get.results.main[i][m].scode);
+
+                 total += parseInt(get.results.main[i][m].marks);
+              }
+                console.log("Total of marks is: ", total);
+                var ctx = document.getElementById("myChart");
+                var data = {
+                  showTooltips: false,
+                  labels: labelsArray,
+                  datasets: [{
+                    label: "# of Marks",
+                    data: marksArray,
+                    backgroundColor: "#ff5233"
+                  }]
+                };
+
+                var myChar = new Chart(ctx, {
+                  type: "bar",
+                  data: data,
+                  options: {
+                    scales: {
+                      yAxes: [{
+                        ticks: {
+                          gridLines: {
+                            display: true,
+                            color: "rgb(255,255,255)"
+                          },
+                          beginAtZero: true
+                        }
+                      }],
+                      xAxes : [{
+                        gridLines: {
+                          display: false
+                        }
+                      }]
+                    }
+                  }
+                });
+
+                get.total = total;
+                var prc = (total/800)*100;
+                get.prc = prc;
+                var grade = "";
+                if (prc >= 75) {
+                  grade = "A";
+                } else if (prc >= 70) {
+                  grade = "B"
+                } else if ((prc <= 69) || (prc >= 59)) {
+                  grade = "C";
+                } else {
+                  grade = "F";
+                }
+                get.grade = grade;
+
                 get.message = "results are Found OK!";
                 return true;
-
-            } else if (search.indexOf(regno) === "" ) {
+            }
+            else if (search.indexOf(regno) === "" ) {
                 get.message = "Enter Your Register Number Please!";
                 return false;
 
              }
-            //  else if (search.toUpperCase().indexOf(regno) === -1) {
-            //     get.message = "The results not found!";
-            //     return true;
-            //   }
               else
               {
                 get.message = "Make sure Register Number is correct, then try again!";
-
           }
         }
     }
 
-
-    // get.total = total;
-    // var prc = (total/800)*100;
-    // get.prc = prc;
-    // var grade = "";
-    // if (prc >= 75) {
-    //   grade = "A";
-    // } else if (prc >= 70) {
-    //   grade = "B"
-    // } else if ((prc <= 69) || (prc >= 59)) {
-    //   grade = "C";
-    // } else {
-    //   grade = "F";
-    // }
-    // get.grade = grade;
   }
   };
-
-
+  // Chart.defaults.global.Chart.defaults.global = '#FFF';
 
 
 function DataService($http, Path) {
